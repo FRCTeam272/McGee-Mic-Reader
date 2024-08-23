2cc6a31ee512626cb490a719953a1bbf66474899
Jake Gadaleta <me@jakegads.dev>
2024-08-23 14:35:38 -0400

    folder rename

--- motor.py
+++ motor.py
@@ -1,41 +1,9 @@
-import RPi.GPIO as GPIO
-import yaml
-config = yaml.safe_load(open("config.yaml"))
 
-power = config['motor_power']
-hello
-AN2 = 13
-AN1 = 12
-DIG2 = 24
-DIG1 = 26
 
-motor = ''
+
 def configure_Pi():
-    global motor
-    global setup_has_run
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(AN2, GPIO.OUT)
-    GPIO.setup(AN1, GPIO.OUT)
-    GPIO.setup(DIG2, GPIO.OUT)
-    GPIO.setup(DIG1, GPIO.OUT)
-    try:
-        motor = GPIO.PWM(AN2, GPIO.HIGH)
-    except:
-        motor = GPIO.PWM(AN1, GPIO.HIGH)
-
+    pass
 def go_forward():
-    global motor
-    try:
-        motor.start(power)
-    except:
-        configure_Pi()
-        motor.start(power)
-
+    pass
 def stop():
-    global motor
-    try:
-        motor.start(0)
-    except:
-        configure_Pi()
-        motor.start(0)
+    pass
