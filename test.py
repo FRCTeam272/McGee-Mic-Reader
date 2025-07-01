9b596938107edc76b2b94e78a08ef97e28e9bbdf
Jake Gadaleta <jake.gads@gmail.com>
2025-07-01 17:53:49 -0400

    added imu bluetooth id and a test suite

---
+++ test.py
@@ -0,0 +1,20 @@
+import yaml
+from witmotion import IMU
+
+with open("config.yaml", 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+bluetooth_address = config['bluetooth_address']
+print("Bluetooth Address:", bluetooth_address)
+
+imu = IMU()
+imu.connect()
+imu.start()
+
+print("IMU connected and started.")
+while True:
+    try:
+        print(imu.get_acceleration())
+    except:
+        print("Error occurred while reading IMU data.")
+        break
